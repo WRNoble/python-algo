1e59c1abfde6a6c71895f6df872b2e8ffd6e0ba8
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-23 11:06:19 -0400

    first commit

---
+++ merge_sort.py
@@ -0,0 +1,11 @@
+class Node:
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class LinkedList:
